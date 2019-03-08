caad9f31337ae385a2a5eaccbd9035524dd6cbd9
Fabiano Adelino <empresario2121@hotmail.com>
2019-03-08 19:28:36 -0300

    Update script1.py

--- script1.py
+++ script1.py
@@ -4,12 +4,15 @@ from flask import Flask
 #Cria uma instância em Flask
 app = Flask(__name__)
 
-#Define rota
+#Define rota e homepage
 @app.route("/")
-
-#Conteudo
 def home():
-    return("Home Page")
+    return render_template("home.html")
+
+#Define 2 rota e conteúdo
+@app.route("/blog")
+def about():
+    return render_template("blog.html")
 
 #Executando e controlando o script
 if (__name__ =="__main__"):
